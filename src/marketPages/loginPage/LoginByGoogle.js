c3b291489c67538b4ece76f445b348493d86c229
David Leung <leungkwinglok@gmail.com>
2024-07-27 13:08:58 +0800

    login page css updated

--- src/marketPages/loginPage/LoginByGoogle.js
+++ src/marketPages/loginPage/LoginByGoogle.js
@@ -1,10 +1,29 @@
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
 import React from 'react'
+import styles from './LoginPage.module.css'
 
 export default function LoginByGoogle() {
+    const Google_Oauth_ClientId = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
+
+    return (
+        <div id="loginInByGoogle">
+            <GoogleOAuthProvider clientId={Google_Oauth_ClientId}>
+                {/* <GoogleLogin
+                    onSuccess={successHandler}
+                    onFailure={failureHandler}
+                    cookiePolicy={'single_host_origin'}
+                    scope="calendar-json.googleapis.com"
+                /> */}
+                <GoogleLoginBtn/>
+                
+            </GoogleOAuthProvider>
+        </div>
+    )
+}
+
+const GoogleLoginBtn = () => {
     const HomePage = process.env.REACT_APP_HOME_PAGE;
     const BackEnd_API = process.env.REACT_APP_BACKEND_API;
-    const Google_Oauth_ClientId = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
 
     const successHandler = (response) => {
         console.log("success by google login: ", response);
@@ -32,16 +51,20 @@ export default function LoginByGoogle() {
         console.log("fail by google login", res);
     }
 
+    const login = useGoogleLogin({
+        onSuccess: {successHandler},
+        onFailure: {failureHandler},
+        cookiePolicy: 'single_host_origin',
+        scope: 'https://www.googleapis.com/auth/calendar'
+    })
+
     return (
-        <div id="loginInByGoogle">
-            <GoogleOAuthProvider clientId={Google_Oauth_ClientId}>
-                <GoogleLogin
-                    onSuccess={successHandler}
-                    onFailure={failureHandler}
-                    cookiePolicy={'single_host_origin'}
-                    scope="calendar-json.googleapis.com"
-                />
-            </GoogleOAuthProvider>
+        <div onClick={() => login()} className={styles.googleLoginBtn}>
+            <img src='/img/google.png' className={styles.googleIcon} alt='googleIcon'/>
+            <div className={styles.googleText}>
+                Continue with Google
+            </div>
         </div>
     )
 }
+ 
